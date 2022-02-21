7a6786fbe8882f03fb73725007814ed79e7eaaf4
angrykoala <angrykoala@outlook.es>
2022-02-21 22:31:41 +0000

    some code cleanup

--- TicTac.py
+++ TicTac.py
@@ -1,4 +1,4 @@
-# TicTac 
+# TicTac
 # by @angrykoala
 
 import time
@@ -13,21 +13,18 @@ single_player=False
 # 1 is x
 # 2 is o
 # 0 is game finished
-player_turn=1 
+player_turn=1
 
 # BITMAP: width: 9, height: 9
 x_bitmap = bytearray([1,130,68,40,16,40,68,130,1,
            1,0,0,0,0,0,0,0,1])
-           
+
 # BITMAP: width: 9, height: 9
 o_bitmap = bytearray([56,198,130,1,1,1,130,198,56,
            0,0,0,1,1,1,0,0,0])
 
 # BITMAP: width: 3, height: 5
 arrow_bitmap = bytearray([31,14,4])
-
-player_sprite_size=9
-
 
 thumby.display.setFPS(25)
 
@@ -41,12 +38,15 @@ x_padding=18
 y_padding=2
 
 flash_speed=800
+
+
+## Drawing functions
+
 def draw_table():
-    total_size=36
+    total_size=square_size*3
     for i in range(1,3):
         thumby.display.drawLine(x_padding+(square_size*i), y_padding, x_padding+(square_size*i), total_size+y_padding, 1)
         thumby.display.drawLine(x_padding, y_padding+(square_size*i), total_size+x_padding,y_padding+(square_size*i), 1)
-     
 
 def flash_square():
     position=get_position(selected_square)
@@ -56,12 +56,23 @@ def get_position(square): # Top left corner of the square
     pos_in_table = [(square%3)*square_size, int(square/3)*square_size]
     return [int(pos_in_table[0]+x_padding), pos_in_table[1]+y_padding]
 
-
 def update_selected_square(value):
     global selected_square
     new_value=selected_square+value
     if new_value>=0 and new_value<=8:
         selected_square=new_value
+
+def draw_players():
+    for square, value in enumerate(table):
+        position=get_position(square)
+        if value==1:
+            draw_player(x_bitmap, position)
+        if value==2:
+             draw_player(o_bitmap, position)
+
+def draw_player(bitmap, position):
+    player_sprite_size=9
+    thumby.display.blit(bitmap, position[0]+2, position[1]+2, player_sprite_size, player_sprite_size, 0, 0, 0)
 
 def user_input():
     global player_turn
@@ -73,80 +84,30 @@ def user_input():
         update_selected_square(-1)
     if thumby.buttonR.justPressed():
         update_selected_square(1)
-        
+
     if thumby.buttonA.justPressed():
         if player_turn==1:
             if table[selected_square]==0:
                 table[selected_square]=1
                 player_turn=2
                 thumby.audio.play(3000, 80)
-    
+
         if player_turn==2 and single_player==False:
             if table[selected_square]==0:
                 table[selected_square]=2
                 player_turn=1
                 thumby.audio.play(3000, 80)
 
-def draw_player(bitmap, position):
-    thumby.display.blit(bitmap, position[0]+2, position[1]+2, player_sprite_size, player_sprite_size, 0, 0, 0)
 
-
-def draw_players():
-    for square, value in enumerate(table):
-        position=get_position(square)
-        if value==1:
-            draw_player(x_bitmap, position)
-        if value==2:
-             draw_player(o_bitmap, position)
-
-# AI is completely stupid, any decent ai would lead to impossible to win game
+# AI is completely stupid, any decent ai would lead to a boring, impossible to win game
 def ai():
     options=[]
     for square, value in enumerate(table):
         if value==0:
             options.append(square)
     return random.choice(options)
-    
-def is_tie():
-    for item in table:
-        if item==0:
-            return False
-    return True
 
-def get_3_in_a_row(player):
-    for i in range(3):
-        row_i=i*3
-        row=[row_i, row_i+1, row_i+2]
-        if is_row_completed(row, player): # rows
-            return row
-        column=[i, i+3, i+6]
-        if is_row_completed(column, player): # column
-            return column
-    return check_diagonals(player)
-
-
-def check_diagonals(player):
-    diagonal1=[0, 4, 8]
-    diagonal2=[2, 4, 6]
-    if is_row_completed(diagonal1, player):
-        return diagonal1
-    if is_row_completed(diagonal2, player):
-        return diagonal2
-    return None
-    
-
-def is_row_completed(row, player):
-    for item in row:
-        if table[item]!=player:
-            return False
-    return True
-
-def draw_3_in_line(row):
-    half_square=int(square_size/2)
-    source=get_position(row[0])
-    target=get_position(row[2])
-    thumby.display.drawLine(source[0]+half_square, source[1]+half_square, target[0]+half_square, target[1]+half_square, 1)
-    
+## Game Result Checks
 
 def check_endgame():
     player_1_row=get_3_in_a_row(1)
@@ -164,6 +125,45 @@ def check_endgame():
         return 0
     return -1
 
+def is_tie():
+    for item in table:
+        if item==0:
+            return False
+    return True
+
+def get_3_in_a_row(player):
+    for i in range(3):
+        row_i=i*3
+        row=[row_i, row_i+1, row_i+2]
+        if is_row_completed(row, player): # rows
+            return row
+        column=[i, i+3, i+6]
+        if is_row_completed(column, player): # column
+            return column
+    return check_diagonals_3_in_a_row(player)
+
+def check_diagonals_3_in_a_row(player):
+    diagonal1=[0, 4, 8]
+    diagonal2=[2, 4, 6]
+    if is_row_completed(diagonal1, player):
+        return diagonal1
+    if is_row_completed(diagonal2, player):
+        return diagonal2
+    return None
+
+def is_row_completed(row, player):
+    for item in row:
+        if table[item]!=player:
+            return False
+    return True
+
+def draw_3_in_line(row):
+    half_square=int(square_size/2)
+    source=get_position(row[0])
+    target=get_position(row[2])
+    thumby.display.drawLine(source[0]+half_square, source[1]+half_square, target[0]+half_square, target[1]+half_square, 1)
+
+
 def display_centered(text, height, show_square=False):
     font_width=7
     size=len(text)*font_width
@@ -172,6 +172,8 @@ def display_centered(text, height, show_square=False):
         thumby.display.drawRectangle(11,0,46, 11, 1)
     thumby.display.drawText(text, int((thumby.display.width/2)-(size/2)), height, 1)
 
+
+## Main Menu
 show_main_menu=True
 menu_option=1
 def main_menu():
@@ -181,29 +183,29 @@ def main_menu():
     global player_turn
     display_centered("TIC TAC", 2, False)
     display_centered("by @angrykoala", 12)
-    
+
 
     arrow_positon=23
     if menu_option==2:
         arrow_positon=33
-        
+
     thumby.display.blit(arrow_bitmap, 6,arrow_positon, 3,5, 0, 0, 0)
-    
+
     thumby.display.drawText("1 Player", 12, 22, 1)
     thumby.display.drawText("2 Players", 12, 32, 1)
-    
+
     if thumby.buttonU.justPressed():
         menu_option=1
     if thumby.buttonD.justPressed():
         menu_option=2
-    
+
     if thumby.buttonA.justPressed():
         show_main_menu=False
         if menu_option==1:
             single_player=True
         elif menu_option==2:
             single_player=False
-        
+
         random.seed(time.ticks_ms())
         player_turn=random.randint(1,2)
         print("Player "+str(player_turn)+" starts")
@@ -215,8 +217,8 @@ def main():
         action=ai()
         table[action]=2
         player_turn=1
-    
-    
+
+
     if player_turn!= 0:
         user_input()
         t0 = time.ticks_ms()
@@ -224,11 +226,11 @@ def main():
             flash_square()
     draw_table()
     draw_players()
-    
+
     endgame=check_endgame()
     if endgame!=-1:
         player_turn=0
-    
+
     if player_turn==0:
         if thumby.buttonA.justPressed():
             return False
